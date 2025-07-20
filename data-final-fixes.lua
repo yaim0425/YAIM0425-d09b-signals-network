e5aae0e037fee51401cff084ed8d12d13639803d
yaim0425 <yaim0425@gmail.com>
2025-07-20 11:15:02 -0500

    graphics

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -36,7 +36,7 @@ function This_MOD.setting_mod()
 
     --- Contenedor
     This_MOD.ref = "radar"
-    This_MOD.tech = data.raw.technology["space-science-pack"]
+    This_MOD.tech = "satellite"
     This_MOD.new_name = This_MOD.prefix .. "transceiver"
     This_MOD.graphics = "__" .. This_MOD.prefix .. This_MOD.name .. "__/graphics/"
     This_MOD.icons = { { icon = This_MOD.graphics .. "item.png" } }
