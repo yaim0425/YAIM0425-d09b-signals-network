29452f9aa5b7bd7ead5ef30f50a562d6916c3c5a
yaim0425 <yaim0425@gmail.com>
2025-10-04 19:07:02 -0500

    Corrección e implementación This_MOD.create_subgroup y This_MOD.create_item

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -33,20 +33,21 @@ function This_MOD.start()
     --- Obtener los elementos
     This_MOD.get_elements()
 
-    -- --- Modificar los elementos
-    -- for _, spaces in pairs(This_MOD.to_be_processed) do
-    --     for _, space in pairs(spaces) do
-    --         --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Modificar los elementos
+    for _, spaces in pairs(This_MOD.to_be_processed) do
+        for _, space in pairs(spaces) do
+            --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-    --         -- --- Crear los elementos
-    --         -- This_MOD.create_item(space)
-    --         -- This_MOD.create_entity(space)
-    --         -- This_MOD.create_recipe(space)
-    --         -- This_MOD.create_tech(space)
+            --- Crear los elementos
+            This_MOD.create_subgroup(space)
+            This_MOD.create_item(space)
+            This_MOD.create_entity(space)
+            -- This_MOD.create_recipe(space)
+            -- This_MOD.create_tech(space)
 
-    --         --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-    --     end
-    -- end
+            --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+        end
+    end
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 end
@@ -101,7 +102,7 @@ function This_MOD.setting_mod()
 
     --- Nombre de las entidades
     This_MOD.name_sender = GMOD.name .. "-" .. This_MOD.id_sender .. "-sender"
-    This_MOD.name_receiver = GMOD.name .. "-" .. This_MOD.id_sender .. "-receiver"
+    This_MOD.name_receiver = GMOD.name .. "-" .. This_MOD.id_receiver .. "-receiver"
 
     --- Ruta a los multimedias
     This_MOD.path_graphics = "__" .. This_MOD.prefix .. This_MOD.name .. "__/graphics/"
@@ -127,7 +128,14 @@ function This_MOD.get_elements()
 
     local Space = {}
     Space.combinator = GMOD.entities["decider-combinator"]
+    Space.item = GMOD.get_item_create(Space.combinator, "place_result")
     Space.entity = GMOD.entities["radar"]
+
+    Space.recipe = GMOD.recipes[Space.item.name]
+    Space.tech = GMOD.get_technology(Space.recipe)
+    Space.recipe = Space.recipe and Space.recipe[1] or nil
+
+    Space.subgroup = This_MOD.prefix .. GMOD.delete_prefix(Space.item.subgroup)
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
@@ -162,7 +170,7 @@ end
 
 ---------------------------------------------------------------------------
 
-function This_MOD.create_item(space)
+function This_MOD.create_subgroup(space)
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
     --- Validación
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
@@ -176,71 +184,31 @@ function This_MOD.create_item(space)
 
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-    --- Duplicar el elemento
+    --- Crear un nuevo subgrupo
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-    local Item = GMOD.copy(space.item)
-
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-
-
-
-
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-    --- Cambiar algunas propiedades
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-    --- Nombre
-    Item.name = space.name
-
-    --- Apodo y descripción
-    Item.localised_name = GMOD.copy(space.entity.localised_name)
-    Item.localised_description = GMOD.copy(This_MOD.lane_splitter.localised_description)
-
-    --- Entidad a crear
-    Item.place_result = space.name
-
-    --- Agregar indicador del MOD
-    table.insert(Item.icons, This_MOD.indicator)
-
-    --- Actualizar Order
-    local Order = tonumber(Item.order) + 1
-    Item.order = GMOD.pad_left_zeros(#Item.order, Order)
-
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-
-
-
-
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-    ---- Crear el prototipo
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-    GMOD.extend(Item)
+    local Old = space.item.subgroup
+    local New = space.subgroup
+    GMOD.duplicate_subgroup(Old, New)
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 end
 
-function This_MOD.create_entity(space)
+function This_MOD.create_item(space)
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-    --- Validación
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-    if not space.entity then return end
-
+    --- Emisor
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
+    local Sender = GMOD.copy(space.item)
+    Sender.icons = { { icon = This_MOD.path_graphics .. "item-sender.png" } }
+    Sender.subgroup = space.subgroup
+    Sender.order = "010"
 
+    Sender.name = This_MOD.name_sender
+    Sender.place_result = This_MOD.name_sender
 
-
-
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-    --- Duplicar el elemento
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-    local Entity = GMOD.copy(space.entity)
+    Sender.localised_name = { "", { "entity-name." .. Sender.name } }
+    Sender.localised_description = { "", { "entity-description." .. Sender.name } }
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
@@ -249,80 +217,19 @@ function This_MOD.create_entity(space)
 
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-    --- Cambiar algunas propiedades
+    --- Receptor
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-    --- Nombre
-    Entity.name = space.name
+    local Receiver = GMOD.copy(space.item)
+    Receiver.icons = { { icon = This_MOD.path_graphics .. "item-receiver.png" } }
+    Receiver.subgroup = space.subgroup
+    Sender.order = "020"
 
-    --- Apodo y descripción
-    Entity.localised_name = GMOD.copy(space.entity.localised_name)
-    Entity.localised_description = GMOD.copy(This_MOD.lane_splitter.localised_description)
+    Receiver.name = This_MOD.name_receiver
+    Receiver.place_result = This_MOD.name_receiver
 
-    --- Cambiar el tipo
-    Entity.type = This_MOD.lane_splitter.type
-
-    --- Elimnar propiedades inecesarias
-    Entity.factoriopedia_simulation = nil
-
-    --- Cambiar icono
-    Entity.icons = GMOD.copy(space.item.icons)
-    table.insert(Entity.icons, This_MOD.indicator)
-
-    --- Copiar algunos valores
-    for _, propiety in pairs({
-        "collision_box",
-        "selection_box",
-        "fast_replaceable_group"
-    }) do
-        Entity[propiety] = This_MOD.lane_splitter[propiety]
-    end
-
-    for _, propiety in pairs({ "structure", "structure_patch" }) do
-        for key, newTable in pairs(Entity[propiety] or {}) do
-            local oldTable = This_MOD.lane_splitter[propiety][key] or {}
-            newTable.shift = oldTable.shift
-            newTable.scale = oldTable.scale
-        end
-    end
-
-    --- Objeto a minar
-    Entity.minable.results = { {
-        type = "item",
-        name = space.name,
-        amount = 1
-    } }
-
-    --- Siguiente tier
-    Entity.next_upgrade = (function()
-        --- Validación
-        if not Entity.next_upgrade then return end
-
-        --- Procesar el nombre
-        local That_MOD =
-            GMOD.get_id_and_name(Entity.next_upgrade) or
-            { ids = "-", name = Entity.next_upgrade }
-
-        --- Nombre despues del aplicar el MOD
-        local New_name =
-            GMOD.name .. That_MOD.ids ..
-            This_MOD.id .. "-" ..
-            That_MOD.name
-
-        --- La entidad ya existe
-        if GMOD.entities[New_name] ~= nil then
-            return New_name
-        end
-
-        --- La entidad existirá
-        for _, Spaces in pairs(This_MOD.to_be_processed) do
-            for _, Space in pairs(Spaces) do
-                if Space.entity.name == Entity.next_upgrade then
-                    return New_name
-                end
-            end
-        end
-    end)()
+    Receiver.localised_name = { "", { "entity-name." .. Receiver.name } }
+    Receiver.localised_description = { "", { "entity-description." .. Receiver.name } }
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
@@ -334,7 +241,346 @@ function This_MOD.create_entity(space)
     --- Crear el prototipo
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-    GMOD.extend(Entity)
+    GMOD.extend(Sender, Receiver)
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+end
+
+function This_MOD.create_entity(space)
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Emisor
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    local Sender = {
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        type = "roboport",
+        name = This_MOD.name_sender,
+
+        localised_name = { "", { "entity-name." .. This_MOD.name_sender } },
+        localised_description = { "", { "entity-description." .. This_MOD.name_sender } },
+
+        icons = { { icon = This_MOD.path_graphics .. "item-sender.png" } },
+
+        collision_box = { { -2.3, -2.3 }, { 2.3, 2.3 } },
+        selection_box = { { -2.5, -2.5 }, { 2.5, 2.5 } },
+        drawing_box = { { -2.5, -2.9 }, { 2.5, 2.5 } },
+
+        max_health = 400,
+
+        energy_usage = "10MW",
+        recharge_minimum = "5MJ",
+        charging_energy = "5MW",
+
+        energy_source = {
+            type = "electric",
+            usage_priority = "secondary-input",
+            input_flow_limit = "1GW",
+            buffer_capacity = "5MJ"
+        },
+
+        base_animation = {
+            layers = {
+                {
+                    filename = This_MOD.path_graphics .. "entity-sender.png",
+                    shift = util.by_pixel(6, -13),
+                    animation_speed = 0.18,
+                    frame_count = 64,
+                    priority = "high",
+                    height = 3232 / 8,
+                    width = 2880 / 8,
+                    line_length = 8,
+                    scale = 0.5
+
+                },
+                {
+                    draw_as_shadow = true,
+                    filename = This_MOD.path_graphics .. "entity-sender-shadow.png",
+                    shift = util.by_pixel(33, 10),
+                    frame_count = 64,
+                    priority = "high",
+                    height = 2224 / 8,
+                    width = 3712 / 8,
+                    line_length = 8,
+                    scale = 0.5
+                }
+            }
+        },
+
+        circuit_connector = {
+            points = {
+                shadow = {
+                    green = { -1.5, 2.2 },
+                    red = { -1.5, 2.2 }
+                },
+                wire = {
+                    green = { -2, 1.7 },
+                    red = { -2, 1.7 }
+                }
+            }
+        },
+
+        minable = {
+            mining_time = 0.2,
+            results = { {
+                type = "item",
+                name = This_MOD.name_sender,
+                amount = 1
+            } }
+        },
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        dying_explosion = "medium-explosion",
+        corpse = "big-remnants",
+        flags = { "placeable-player", "player-creation" },
+
+        logistics_radius = 0,
+        robot_slots_count = 0,
+        construction_radius = 0,
+        material_slots_count = 0,
+        charge_approach_distance = 0,
+
+        draw_logistic_radius_visualization = false,
+        draw_construction_radius_visualization = false,
+
+        radar_range = space.entity.max_distance_of_sector_revealed or 1,
+        request_to_open_door_timeout = 15,
+        spawn_and_station_height = -0.1,
+        circuit_wire_max_distance = 10,
+
+        vehicle_impact_sound = {
+            filename = "__base__/sound/car-metal-impact.ogg",
+            volume = 0.65
+        },
+
+        working_sound = {
+            sound = {
+                filename = "__base__/sound/roboport-working.ogg",
+                volume = 0.6
+            },
+            max_sounds_per_type = 3,
+            audible_distance_modifier = 0.5,
+            probability = 1 / (15 * 60)
+        },
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    }
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Receptor
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    local Receiver = {
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        type = "roboport",
+        name = This_MOD.name_receiver,
+
+        localised_name = { "", { "entity-name." .. This_MOD.name_receiver } },
+        localised_description = { "", { "entity-description." .. This_MOD.name_receiver } },
+
+        icons = { { icon = This_MOD.path_graphics .. "item-receiver.png" } },
+
+        collision_box = { { -4.3, -4.3 }, { 4.3, 4.3 } },
+        selection_box = { { -4.5, -4.5 }, { 4.5, 4.5 } },
+        drawing_box = { { -4.5, -4.9 }, { 4.5, 4.5 } },
+
+        max_health = 800,
+
+        energy_usage = "2MW",
+        recharge_minimum = "1MJ",
+        charging_energy = "1MW",
+
+        energy_source = {
+            type = "electric",
+            usage_priority = "secondary-input",
+            input_flow_limit = "1GW",
+            buffer_capacity = "1MJ"
+        },
+
+        base_animation = {
+            layers = {
+                {
+                    filename = This_MOD.path_graphics .. "entity-receiver.png",
+                    shift = util.by_pixel(1, -26),
+                    animation_speed = 0.15,
+                    frame_count = 64,
+                    priority = "high",
+                    height = 5440 / 8,
+                    width = 4688 / 8,
+                    line_length = 8,
+                    scale = 0.5
+                },
+                {
+                    draw_as_shadow = true,
+                    filename = This_MOD.path_graphics .. "entity-receiver-shadow.png",
+                    shift = util.by_pixel(25, 19),
+                    frame_count = 64,
+                    priority = "high",
+                    height = 4800 / 8,
+                    width = 5440 / 8,
+                    line_length = 8,
+                    scale = 0.5
+                }
+            }
+        },
+
+        circuit_connector = {
+            points = {
+                shadow = {
+                    green = { -2.5, 4.2 },
+                    red = { -2.7, 4 }
+                },
+                wire = {
+                    green = { -3.5, 3.2 },
+                    red = { -3.7, 3 }
+                }
+            }
+        },
+
+        minable = {
+            mining_time = 0.5,
+            results = { {
+                type = "item",
+                name = This_MOD.name_receiver,
+                amount = 1
+            } }
+        },
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        dying_explosion = "medium-explosion",
+        corpse = "big-remnants",
+        flags = { "placeable-player", "player-creation" },
+
+        logistics_radius = 0,
+        robot_slots_count = 0,
+        construction_radius = 0,
+        material_slots_count = 0,
+        charge_approach_distance = 0,
+
+        draw_logistic_radius_visualization = false,
+        draw_construction_radius_visualization = false,
+
+        radar_range = space.entity.max_distance_of_sector_revealed or 1,
+        request_to_open_door_timeout = 15,
+        spawn_and_station_height = -0.1,
+        circuit_wire_max_distance = 10,
+
+        vehicle_impact_sound = {
+            filename = "__base__/sound/car-metal-impact.ogg",
+            volume = 0.65
+        },
+        working_sound = {
+            sound = {
+                filename = "__base__/sound/roboport-working.ogg",
+                volume = 0.6
+            },
+            max_sounds_per_type = 3,
+            audible_distance_modifier = 0.5,
+            probability = 1 / (15 * 60)
+        },
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    }
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Combinador
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    local Direction = {
+        north = util.empty_sprite(),
+        east = util.empty_sprite(),
+        south = util.empty_sprite(),
+        west = util.empty_sprite()
+    }
+
+    local Connection_points = {
+        {
+            shadow = { red = { 0, 0 }, green = { 0, 0 } },
+            wire = { red = { 0, 0 }, green = { 0, 0 } }
+        },
+        {
+            shadow = { red = { 0, 0 }, green = { 0, 0 } },
+            wire = { red = { 0, 0 }, green = { 0, 0 } }
+        },
+        {
+            shadow = { red = { 0, 0 }, green = { 0, 0 } },
+            wire = { red = { 0, 0 }, green = { 0, 0 } }
+        },
+        {
+            shadow = { red = { 0, 0 }, green = { 0, 0 } },
+            wire = { red = { 0, 0 }, green = { 0, 0 } }
+        }
+    }
+
+    local Light_offsets = {
+        { 0, 0 },
+        { 0, 0 },
+        { 0, 0 },
+        { 0, 0 }
+    }
+
+    local Combinator = {
+        type = "decider-combinator",
+        name = This_MOD.prefix .. space.combinator.name,
+
+        localised_name = "",
+        localised_description = "",
+
+        icons = { { icon = "__base__/graphics/icons/decider-combinator.png" } },
+
+        collision_box = { { 0, 0 }, { 0, 0 } },
+        selection_box = { { 0, 0 }, { 0, 0 } },
+
+        max_health = 1,
+
+        energy_source = { type = "void" },
+        active_energy_usage = "1W",
+
+        circuit_wire_max_distance = 9,
+        selectable_in_game = false,
+        hidden = true,
+        flags = { "not-on-map" },
+
+        sprites = Direction,
+        activity_led_sprites = Direction,
+        activity_led_light = { intensity = 0, size = 0 },
+        screen_light_offsets = Light_offsets,
+        activity_led_light_offsets = Light_offsets,
+
+        input_connection_bounding_box = { { -0.5, -0.5 }, { 0.5, 0.5 } },
+        output_connection_bounding_box = { { -0.5, -0.5 }, { 0.5, 0.5 } },
+
+        circuit_wire_connection_points = Connection_points,
+        output_connection_points = Connection_points,
+        input_connection_points = Connection_points
+    }
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Crear el prototipo
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    GMOD.extend(Sender, Receiver, Combinator)
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 end
@@ -535,4 +781,3 @@ end
 This_MOD.start()
 
 ---------------------------------------------------------------------------
-ERROR()
